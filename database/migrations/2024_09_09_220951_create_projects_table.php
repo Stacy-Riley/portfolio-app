151fd3115af444b4e8d7d2002a0d86c1f71c1a4f
stacy_r <stacyrileyca@gmail.com>
2024-09-10 11:22:18 -0500

    Create models, controllers, tables for app(PA-6)

---
+++ database/migrations/2024_09_09_220951_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->boolean('is_published')->default(false);
+            $table->integer('priority')->default(0);
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('body');
+            $table->string('author')->nullable();
+            $table->string('category')->nullable();
+            $table->string('cover_image');
+            $table->string('supporting_images')->nullable();
+            $table->date('publish_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
