c4b05961db5030afd177ed0956c666d1f04d1b7c
stacy_r <stacyrileyca@gmail.com>
2024-10-02 13:40:32 -0500

    Log slow queries while in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
-
+DB::listen(function($query) {
+    if ($query->time > 100) {  // Adjust the threshold to what you consider "slow" (in milliseconds)
+        Log::info('Slow query detected: ' . $query->sql, [
+            'bindings' => $query->bindings,
+            'time' => $query->time,
+        ]);
+    }
+});
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      */
