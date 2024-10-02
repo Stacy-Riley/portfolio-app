ac7b6b45574d34d9fb4a40b7d6816c9afc8c69ac
stacy_r <stacyrileyca@gmail.com>
2024-10-02 14:11:40 -0500

    Added missing providers and aliases mentions to config/app.php

--- config/app.php
+++ config/app.php
@@ -83,6 +83,23 @@ return [
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'providers' => [
+        // Other Service Providers...
+
+        /*
+         * Laravel Framework Service Providers...
+         */
+        Illuminate\View\ViewServiceProvider::class,
+
+        // More Service Providers...
+    ],
+    'aliases' => [
+        // Other Facades...
+
+        'View' => Illuminate\Support\Facades\View::class,
+
+        // More Facades...
+    ],
 
     /*
     |--------------------------------------------------------------------------
